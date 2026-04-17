590f08d934d5c2a64188d41dd4ba12a7d8d75848
Christian Trutz <info@trutz-software-consulting.de>
2026-04-17 21:22:33 +0200

    chore: Update project details in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Kubernetes Tutorial'
+copyright = '2026, Trutz Software Consulting GmbH'
+author = 'Christian Trutz'
 
 release = '0.1'
 version = '0.1.0'
