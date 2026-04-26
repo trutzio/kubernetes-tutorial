bfa70a8bb56681393b8cefe922c5a38e04c49331
Christian Trutz <info@trutz-software-consulting.de>
2026-04-26 16:04:56 +0200

    feat: add rolldice application with installation instructions and requirements

---
+++ src/apps/rolldice/README.md
@@ -0,0 +1,9 @@
+## Installation
+
+```
+python3 -m venv .venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+flask run -p 8080
+curl http://localhost:8080/rolldice
+```
---
+++ src/apps/rolldice/app.py
@@ -0,0 +1,24 @@
+from random import randint
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@app.route("/rolldice")
+def roll_dice():
+    player = request.args.get('player', default=None, type=str)
+    result = str(roll())
+    if player:
+        logger.warning("%s is rolling the dice: %s", player, result)
+    else:
+        logger.warning("Anonymous player is rolling the dice: %s", result)
+    if result == '6':
+        raise Exception("Sorry, you rolled a 6!") 
+    return result
+
+
+def roll():
+    return randint(1, 6)
